ff64efb3166ea46a800a7e1d2fa2b261e28827c3
MUFASA1979 <msintriors@gmail.com>
2023-09-13 08:52:18 +0300

    Day2 BMI Challenge Done

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
-}
+
+  let BMI = weight/(height/100*height/100);
+  window.alert(BMI);
+  
+    if(BMI<18.5){
+      window.alert("Underweight")} 
+      else if(BMI >= 18.5 && BMI <=24.9){
+      window.alert("Healthy Weight")}
+      else if (BMI >= 25.0 && BMI <=29.9){
+      window.alert("Overweight")}
+      else if (BMI >= 30.0){
+      window.alert("Above Obesity")}
+ }
